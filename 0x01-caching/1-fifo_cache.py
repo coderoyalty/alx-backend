cc1105b6da45727381772a5b4fcede4ca6580d0b
Akanni Emmanuel <akannie33@gmail.com>
2023-08-22 04:14:44 -0700

    0x01: task 1 solution

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+fifo cache
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        """
+        initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        add an item to the cache using a key
+        as its index
+        """
+        if not key or not item:
+            pass
+        if len(self.cache_data) == self.MAX_ITEMS:
+            keys_iter = iter(self.cache_data.keys())
+            first_item = next(keys_iter)
+            del self.cache_data[first_item]
+            print(f"DISCARD {first_item}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        get an item from the cache
+        """
+        return self.cache_data.get(key, None)
