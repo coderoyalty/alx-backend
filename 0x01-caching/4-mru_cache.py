c15c866be2191195097609f86cd63d4e035f0346
Akanni Emmanuel <akannie33@gmail.com>
2023-08-22 14:22:44 -0700

    0x01: task 4 complete

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+most recently used (lru) cache
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU Cache Method Implementation
+    """
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        add an item to the cache using a key
+        as its index
+        """
+        if not key or not item:
+            return
+        keys = self.cache_data.keys()
+
+        if len(self.cache_data) == self.MAX_ITEMS and key not in keys:
+            rm_key = self.order.pop(0)
+            self.cache_data.pop(rm_key)
+            print(f"DISCARD: {rm_key}")
+        if key in keys:
+            self.order.remove(key)
+        self.cache_data[key] = item
+        self.order.insert(0, key)
+
+    def get(self, key):
+        """
+        get an item from the cache
+        """
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.insert(0, key)
+
+        return self.cache_data.get(key, None)
