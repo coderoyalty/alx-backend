692fcf3b8ba226c5ccfff3d7199773d9735af886
Akanni Emmanuel <akannie33@gmail.com>
2023-08-22 12:13:31 -0700

    0x01: task 2 fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -29,7 +29,7 @@ class LIFOCache(BaseCaching):
             keys = list(keys)
             rm_key = keys[-1]
             self.cache_data.pop(rm_key)
-            print(f"DISCARD {rm_key}")
+            print(f"DISCARD: {rm_key}")
         self.cache_data[key] = item
 
     def get(self, key):
