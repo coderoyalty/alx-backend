192f3926a38896fc986fa6a84e36a80a91ad1455
Akanni Emmanuel <akannie33@gmail.com>
2023-08-22 02:03:20 -0700

    0x01: task 0 done

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+basic cache
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self, *args, **kwargs):
+        """
+        initialize
+        """
+        super().__init__(*args, **kwargs)
+
+    def put(self, key, item):
+        """
+        place an item in the cache dict using its
+        key as index
+        """
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        fetch an item from the cache
+        """
+        return self.cache_data.get(key, None)
